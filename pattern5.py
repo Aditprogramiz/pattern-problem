22e36f6d826908303450b39b98e30af0440b54f3
aditya kashyap <24f3004267@ds.study.iitm.ac.in>
2025-07-17 20:48:17 +0530

    Add files via upload

---
+++ pattern5.py
@@ -0,0 +1,12 @@
+r=5
+for i in range(r,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+    '''
+    *****
+    ****
+    ***
+    **
+    *
+    '''    
---
+++ pattern7.py
@@ -0,0 +1,7 @@
+r=5
+for i in range(5):
+    print(" "*(r-i),end="")
+    for j in range(i+1):
+         print("*",end=" ")
+    print()    
+    #diamond
